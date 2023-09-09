50de5243bbf55d747fbc26003a2ca665b5b83e00
David Figueroa <davidfigueroa0327@gmail.com>
2023-09-09 10:15:01 -0500

    update dark mode with css variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
 const App = () => {
-  useEffect(() => {
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    if (isDarkMode) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-  }, [window.matchMedia('(prefers-color-scheme: dark)').matches]);
-
   return <RouterProvider router={router} />;
 };
 
